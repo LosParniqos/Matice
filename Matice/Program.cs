3501fc2bc6878f947087b562bae7f7fc3aa5ce39
Lukáš Soustružník <lukas.soustruznik@student.gmh.cz>
2023-03-16 15:35:35 +0100

    v0.2

--- Matice/Program.cs
+++ Matice/Program.cs
@@ -9,46 +9,84 @@ namespace Matice
             Console.WriteLine("Kolik ma graf vrcholu?");
             int vrcholy = int.Parse(Console.ReadLine());
 
-            int INF = int.MaxValue;
-            int[,] matice = new int[vrcholy, vrcholy];
-
-            for (int x = 0; x < vrcholy; x++)
+            int INF = int.MaxValue / 2;
+            int[,] matice =
             {
-                for (int y = 0; y < vrcholy; y++)
-                {
+            {0, 5, INF, 10},
+            {INF, 0, 3, 1},
+            {INF, INF, 0, 1},
+            {INF, INF, INF, 0},
+            };
 
-                    if (x == y)
+
+            FloydWarshall();
+            void FloydWarshall()
+            {
+
+                for (int i = 0; i < vrcholy; i++)
+                {
+                    for (int j = 0; j < vrcholy; j++)
                     {
-                        matice[x, y] = INF;
-                        continue;
-                    }
-                    Console.WriteLine("Jakou vahu ma propoj {0} s {1}?", x + 1, y + 1);
-                    int VahaSpoje = int.Parse(Console.ReadLine());
-                    if (VahaSpoje == 0)
-                    {
-                        matice[x, y] = INF;
-                    }
-                    else
-                    {
-                        matice[x, y] = VahaSpoje;
+                        for (int k = 0; k < vrcholy; k++)
+                        {
+                            matice[i, j] = Math.Min(matice[i, j], matice[i, k] + matice[k, j]);
+                        }
                     }
                 }
+                Console.WriteLine("Odkud chceš cestu?");
+                int odkud = int.Parse(Console.ReadLine());
+                Console.WriteLine("Kam chceš cestu?");
+                int kam = int.Parse(Console.ReadLine());
+                Console.WriteLine(matice[odkud - 1, kam - 1]);
             }
-            for (int x = 0; x < vrcholy; x++)
+
+
+
+          /*  int[,] matice = new int[vrcholy, vrcholy];
+            
+            InicializaceGrafu();
+
+           void InicializaceGrafu()
             {
-                Console.WriteLine();
-                for (int y = 0; y < vrcholy; y++)
+                for (int x = 0; x < vrcholy; x++)
                 {
-                    if (matice[x, y] == INF)
+                    for (int y = 0; y < vrcholy; y++)
                     {
-                        Console.Write("MAX".ToString().PadLeft(5));
-                    }
-                    else
-                    {
-                        Console.Write(matice[x, y].ToString().PadLeft(5));
+
+                        if (x == y)
+                        {
+                            matice[x, y] = INF;
+                            continue;
+                        }
+                        Console.WriteLine("Jakou vahu ma propoj {0} s {1}?", x + 1, y + 1);
+                        int VahaSpoje = int.Parse(Console.ReadLine());
+                        if (VahaSpoje == 0)
+                        {
+                            matice[x, y] = INF;
+                        }
+                        else
+                        {
+                            matice[x, y] = VahaSpoje;
+                        }
                     }
                 }
-            }
+                for (int x = 0; x < vrcholy; x++)
+                {
+                    Console.WriteLine();
+                    for (int y = 0; y < vrcholy; y++)
+                    {
+                        if (matice[x, y] == INF)
+                        {
+                            Console.Write("MAX".ToString().PadLeft(5));
+                        }
+                        else
+                        {
+                            Console.Write(matice[x, y].ToString().PadLeft(5));
+                        }
+                    }
+                }
+            }*/
+
         }
     }
 }
